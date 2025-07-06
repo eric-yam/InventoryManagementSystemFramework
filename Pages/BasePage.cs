8310d455ef42e39f11ca3816e3955095a4009e10
ericyam-git <70084832+ericyam-git@users.noreply.github.com>
2025-07-06 14:21:40 -0400

    Adjusted Base Page to include Sidebar component

--- Pages/BasePage.cs
+++ Pages/BasePage.cs
@@ -5,10 +5,12 @@ namespace InventoryManagementSystemFramework.Pages
     public abstract class BasePage
     {
         public IPage page;
+        public SideBar sideBar;
 
         public BasePage(IPage page)
         {
             this.page = page;
+            this.sideBar = new SideBar(this.page);
         }
     }
 }
